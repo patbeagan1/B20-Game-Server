eadadb6412d7f2341444b3207dff2d71fba90752
patbeagan1 <pbeagan1@gmail.com>
2023-01-02 20:50:04 -0600

    Pulling out a module

--- server/console-vision/src/main/java/com/pbeagan/consolevision/Color256.kt
+++ server/console-vision/src/main/java/com/pbeagan/consolevision/Color256.kt
@@ -1,8 +1,22 @@
 package com.pbeagan.consolevision
 
-import com.pbeagan.util.Util.distance
 import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
+fun distance(
+    x1: Int,
+    y1: Int,
+    z1: Int,
+    x2: Int,
+    y2: Int,
+    z2: Int
+): Double {
+    val d1 = (x2 - x1).toDouble().pow(2)
+    val d2 = (y2 - y1).toDouble().pow(2)
+    val d3 = (z2 - z1).toDouble().pow(2)
+    return sqrt(d1 * d2 * d3)
+}
 enum class Color256(val color: Int, val number: Int) {
 
     Color0(0x000000C, 0),
--- server/src/com/pbeagan/util/Util.kt
+++ server/src/com/pbeagan/util/Util.kt
@@ -9,20 +9,6 @@ import kotlin.math.sqrt
 
 object Util {
 
-    fun distance(
-        x1: Int,
-        y1: Int,
-        z1: Int,
-        x2: Int,
-        y2: Int,
-        z2: Int
-    ): Double {
-        val d1 = (x2 - x1).toDouble().pow(2)
-        val d2 = (y2 - y1).toDouble().pow(2)
-        val d3 = (z2 - z1).toDouble().pow(2)
-        return sqrt(d1 * d2 * d3)
-    }
-
     fun distance(xy1: Coord, xy2: Coord) = distance(
         xy1.x.toDouble(),
         xy1.y.toDouble(),
